e53312634002f6340f52dd951e8341bffb39862e
Preunicom <44133097+Preunicom@users.noreply.github.com>
2025-03-22 12:49:35 +0100

    Refactored and extended TB_Main_Unit

--- README.md
+++ README.md
@@ -4,96 +4,108 @@
 The ExtPack is able to add units (like GPIO, Timer or UART) to a UART capable device.
 
 ## Communication with Host
-The ExtPack communicates with the host over UART. <br>
+The ExtPack communicates with the host over UART.  
 The communication with the host has to be at least 8 data bits.
 Stop bits and parity bit can be set as wished.
 
 ### Protocol with host
-The ExtPack uses a custom protocol which uses UART as base. <br>
-Each host to ExtPack data package consists of two UART packages following each other in less then three UART package cycles. <br>
-The first one includes the unit number to communicate with (bits 5 downto 0) and the access mode for the unit (bits 7 downto 6). If there are more then 8 host UART data bits the following bits are ignored.<br>
-The second one includes the data for the unit. If the unit uses less then your host UART data bits, the top bits are truncated in the unit wrapper (same the other way round if the unit has more bits than the host UART data bits). <br>
+The ExtPack uses a custom protocol which uses UART as base.  
+Each host to ExtPack data package consists of two UART packages following each other in less then three UART package cycles.  
+The first one includes the unit number to communicate with (bits 5 downto 0) and the access mode for the unit (bits 7 downto 6). If there are more then 8 host UART data bits the following bits are ignored.  
+The second one includes the data for the unit. If the unit uses less then your host UART data bits, the top bits are truncated in the unit wrapper (same the other way round if the unit has more bits than the host UART data bits).  
 In the other direction from ExtPack to host the protocol is nearly the same with the difference of missing the access mode of the first UART package. These bits are always zero.
 
 ## Usage
 ### Declare and define units
 Units are declared in the Main_Unit between "UNITS" and "UNITS END".
 the first generic and the first 8 ports are the same for all units.
-Following generics and ports are unit specific. <br>
+Following generics and ports are unit specific.  
 The specific ports have to be additinally declared in the constraints file and in the entity of Main_Unit between "UNIT PORTS" and "UNIT PORTS END". 
-(Note: The last line before "UNIT PORTS END" must not have a semicolon at the end!)
+**Note:** The last line before "UNIT PORTS END" must not have a semicolon at the end!
 
 ### Define host communication
-Set the default values of Main_Unit to your specific UART configuration of your host. <br>
-(Note: Make sure you have >= 8 data bits for your host communication as well as a BAUD rate less or equal of half your FPGA frequency)
+Set the default values of Main_Unit to your specific UART configuration of your host.  
+**Note:** Make sure you have >= 8 data bits for your host communication as well as a BAUD rate less or equal of half your FPGA frequency
 
 ## Units
 ### UART_Unit
-Can be configured with BAUD rate, data bits, stop bits and parity bit. <br>
-Needs two pins (rx and tx) of the FPGA. <br>
-BAUD has to be less or equal than half of the FGGA frequency. (Note: Integer divisor baud rates lead to more stable UART communication) <br>
-Data bits have to be more than 5 and all bits (stop, data and parity) have to be less or equal 15. <br>
+Can be configured with BAUD rate, data bits, stop bits and parity bit.  
+Needs two pins (rx and tx) of the FPGA.  
+BAUD has to be less or equal than half of the FGGA frequency. 
+**Note:** Integer divisor baud rates lead to more stable UART communication  
+Data bits have to be more than 5 and all bits (stop, data and parity) have to be less or equal 15.  
 Normally: 
   - data bits: 5-9
   - stop bits: 1-2
   - parity bit: 0-1
 
-UART messages are directly forwarded from unit to the host or from the host to the unit. <br>
-Access mode is ignored. <br>
-Note: UART messages with parity or frame errors are ignorer! <br>
-Note: If there is too much traffic on the ExtPack and UART Unit has to less priority and is receiving too much load it is possible that UART packages are getting lost because it is scheduled too slow or never because of starvation.
+UART messages are directly forwarded from unit to the host or from the host to the unit.  
+Access mode is ignored.  
+**Note:** UART messages with parity or frame errors are ignorer!  
+**Note:** If there is too much traffic on the ExtPack and UART Unit has to less priority and is receiving too much load it is possible that UART packages are getting lost because it is scheduled too slow or never because of starvation.
 The system operates on a Best-Effort Delivery basis, meaning it strives to transmit data as efficiently as possible but does not guarantee delivery.
 
 ### GPIO_Unit
-Can be configured with in and out pin amount. <br>
+Can be configured with in and out pin amount.  
 Pin amount has to be at least 1 and maximum the amount of data bits of the host UART communication.
 The access mode controls the type of pins.
   - "00" or "10": Set output pins
   - "01" or "11": Request values of input pins
 
-If there is an interrupt on a input pin detected a message with the pin values is sent to the host. <br>
-Note: Debouncing is not prevented. <br>
-Note: The output pins are set to the given values from the host. There is no way to only set **one** specific pin to a value.
+If there is an interrupt on a input pin detected a message with the current pin values is sent to the host.  
+**Note:** Debouncing is not prevented. Therefore its possible to lose interrupts if they are faster than the scheduling and the UART transmission to the host.  
+**Note:** The output pins are set to the given values from the host. There is no way to only set **one** specific pin to a value.
 
 ## Timer_Unit
-Can be configured via UART (No configuaration in VHDL code neccessary). <br>
-The timer is a x-bit timer with x being the amount of bits of the host UART communication. <br>
-It counts from a given start value (default 0) up to the maximum value of x bit. The overflow triggers an interrupt which is sent to the host. <br>
-The timer frequency is at 5% of host baud rate. <br>
-Note: The reason is, that that is the maximum of ExtPack packages (consists of two UART packages: unit number and unit data) that are be able to be transmitted to the host via 8N1 UART, which is the fastest supported host UART mode when looking at packages transmission rate. <br>
-The speed of counting (prescaler) can also be set as a divisor of this 5% of host BAUD frequency. (default: 1) <br>
-For example: With a host baud rate of 1 MHz a prescale divisor of 2 results in 25 KHz. <br>
+Can be configured via UART (No configuaration in VHDL code neccessary).  
+The timer is a x-bit timer with x being the amount of bits of the host UART communication.  
+It counts from a given start value (default 0) up to the maximum value of x bit. The overflow triggers an interrupt which is sent to the host.  
+The timer frequency is at 5% of host baud rate.  
+**Note:** The reason is, that that is the maximum of ExtPack packages (consists of two UART packages: unit number and unit data) that are be able to be transmitted to the host via 8N1 UART, which is the fastest supported host UART mode when looking at packages transmission rate.  
+The speed of counting (prescaler) can also be set as a divisor of this 5% of host BAUD frequency. (default: 1)  
+For example: With a host baud rate of 1 MHz a prescale divisor of 2 results in 25 KHz.  
 The access mode handles all this configurations: 
  - "00": Enables/disables the timer. 
     - 0 as value disables the timer.
     - Any value greater then 0 enables the timer.
  - "01": Restarts the timer. (value is ignored)
- - "10": Sets the value as the prescale divisor. (Note: Even values lead to a preciser timer)
+ - "10": Sets the value as the prescale divisor. (**Note:** Even values lead to a preciser timer)
  - "11": Sets the value as the start value of the timer.
 
- Note: Think of the fact, that scheduling and sending the timer overflow interrupt to the host will need some time.
+ **Note:** Think of the fact, that scheduling and sending the timer overflow interrupt to the host will need some time.  
+ **Note:** As the timer counts even if disabled, because disabling only targets the interrupt, you have to restart the timer to apply the set start value and get the result as expected.  
+ 
+ **Timer init suggestion:**
+ 1) set prescale factor and or start value (the order doesn't matter if the timer is restarted afterwards)
+ 2) restart the timer
+ 3) enable the timer
+
+**Timer change suggestion:**
+ 1) disable the timer
+ 2) init the timer like described above
+
 
 ## Internal structure
 ### Incoming data from host
 (1) UART_Unit -> (2) Decoder -> (3) MUX -> (4) Unit
-1) **UART_Unit** <br>
+1) **UART_Unit**  
 Receives the data via UART from rx_pin_host.
-2) **Decoder** <br>
+2) **Decoder**  
 Decodes the received data in unit_number, access_mode and unit_data.
-3) **MUX** <br>
+3) **MUX**  
 Sends the enable signal to the unit with the decoded unit_number.
-4) **Unit** <br>
+4) **Unit**  
 Performs unit specific actions with the received data.
 
 ### Outgoing data to host
 (1) Unit -> (2) PriorityScheduler -> (3) DEMUX -> (4) Encoder -> (5) UART_Unit
-1) **Unit** <br>
+1) **Unit**  
 Provides the data to send to the host.
-2) **PriorityScheduler** <br>
+2) **PriorityScheduler**  
 Schedules the units with their priority (higher unit_number is less priority).
-3) **DEMUX** <br>
+3) **DEMUX**  
 Choose the unit_data chosen from PriorityScheduler and gives it to the Encoder.
-4) **Encoder** <br>
+4) **Encoder**  
 Encodes the data in the protocoll.
-5) **UART_Unit** <br>
+5) **UART_Unit**  
 Sends the data to the host over UART.
--- UART_Extension_Pack.srcs/sim_1/new/TB_Main_Unit.vhd
+++ UART_Extension_Pack.srcs/sim_1/new/TB_Main_Unit.vhd
@@ -31,17 +31,25 @@ architecture TESTBENCH of TB_Main_Unit is
       --------------- UNIT PORTS END ---------------
     );
   end component;
-  constant tbase: time := 10 ns;
-  signal tb_clk, tb_rst, tb_tx_pin_host, tb_tx_pin_a, tb_rx_pin_host, tb_rx_pin_a : std_logic;
+  signal tb_clk : STD_LOGIC;
+  signal tb_rst : STD_LOGIC;
+  
+  signal tb_tx_pin_host, tb_exp_tx_pin_host : STD_LOGIC;
+  signal tb_tx_pin_a, tb_exp_tx_pin_a : STD_LOGIC;
+  signal tb_rx_pin_host : STD_LOGIC;
+  signal tb_rx_pin_a : std_logic;
   signal tb_gpio_pins_in : std_logic_vector(0 downto 0);
-  signal tb_gpio_pins_out : std_logic_vector(1 downto 0);
-begin
-  MU: Main_Unit generic map(100000000, 50000000, 8, 1, 0, 0) port map(tb_clk, tb_rst, tb_tx_pin_host, tb_rx_pin_host, tb_tx_pin_a, tb_rx_pin_a, tb_gpio_pins_in, tb_gpio_pins_out);
-  -- You have to set unit_U00 to 50000000 BAUD in Main_Unit if debuging U00_UART
+  signal tb_gpio_pins_out, tb_exp_gpio_pins_out : std_logic_vector(1 downto 0);
 
-  CLK: process
+  constant tbase : time := 100 ns;
+  signal tb_error : std_logic;
+begin
+  MU: Main_Unit generic map(10000000, 1000000, 8, 1, 1, 0) port map(tb_clk, tb_rst, tb_tx_pin_host, tb_rx_pin_host, tb_tx_pin_a, tb_rx_pin_a, tb_gpio_pins_in, tb_gpio_pins_out);
+  
+  -- 10 MHz
+  CLOCK: process
   begin
-    for i in 1000 downto 0 loop
+    for i in 5000 downto 0 loop
       tb_clk <= '1';
       wait for tbase/2;
       tb_clk <= '0';
@@ -51,21 +59,54 @@ begin
   end process;
 
   tb_rst <= '1', '0' after 1*tbase;
+
   tb_rx_pin_host <= '1',
-                    '0' after 10*tbase, '1' after 12*tbase, '0' after 14*tbase, '1' after 18*tbase, '0' after 20*tbase, '1' after 28*tbase, --0b00001001 (get GPIO data)
-                    '0' after 30*tbase, '1' after 32*tbase, --0b11111111
-                    '0' after 110*tbase, '1' after 112*tbase, '0' after 114*tbase, '1' after 128*tbase, --00000001 (set GPIO data)
-                    '0' after 140*tbase, '1' after 144*tbase, --0b11111110
-                    '0' after 200*tbase, '1' after 218*tbase, --00000000 (send UART)
-                    '0' after 240*tbase,  '1' after 246*tbase, --0b11111100
-                    '0' after 300*tbase, '1' after 318*tbase, --00000000 (send UART)
-                    '0' after 340*tbase,  '1' after 350*tbase, --0b11110000
-                    '0' after 400*tbase, '1' after 404*tbase, '0' after 406*tbase, '1' after 418*tbase, --00000010 (enable Timer)
-                    '0' after 430*tbase,  '1' after 432*tbase; --0b11111111
-  tb_rx_pin_a <= '1';
-  --               '0' after 110*tbase, '1' after 120*tbase, '0' after 122*tbase, '1' after 128*tbase; --0b00010000
+    '0' after 10*tbase, '1' after 20*tbase, '0' after 30*tbase, '0' after 40*tbase, '0' after 50*tbase, '0' after 60*tbase, '0' after 70*tbase, '1' after 80*tbase, '0' after 90*tbase, '0' after 100*tbase, '1' after 110*tbase, --0b01000001 (get GPIO data) (0x41)
+    '0' after 210*tbase, '1' after 220*tbase, '1' after 230*tbase, '1' after 240*tbase, '1' after 250*tbase, '1' after 260*tbase, '1' after 270*tbase, '1' after 280*tbase, '1' after 290*tbase, '0' after 300*tbase, '1' after 310*tbase, --0b11111111 (0xFF)
+    '0' after 410*tbase, '1' after 420*tbase, '0' after 430*tbase, '0' after 440*tbase, '0' after 450*tbase, '0' after 460*tbase, '0' after 470*tbase, '0' after 480*tbase, '0' after 490*tbase, '1' after 500*tbase, '1' after 510*tbase, --00000001 (set GPIO data) (0x01)
+    '0' after 610*tbase, '0' after 620*tbase, '1' after 630*tbase, '1' after 640*tbase, '1' after 650*tbase, '1' after 660*tbase, '1' after 670*tbase, '1' after 680*tbase, '1' after 690*tbase, '1' after 700*tbase, '1' after 710*tbase, --0b11111110 (0xFE)
+    '0' after 810*tbase, '0' after 820*tbase, '0' after 830*tbase, '0' after 840*tbase, '0' after 850*tbase, '0' after 860*tbase, '0' after 870*tbase, '0' after 880*tbase, '0' after 890*tbase, '0' after 900*tbase, '1' after 910*tbase, --00000000 (send UART) (0x00)
+    '0' after 1010*tbase, '1' after 1020*tbase, '0' after 1030*tbase, '1' after 1040*tbase, '1' after 1050*tbase, '1' after 1060*tbase, '1' after 1070*tbase, '1' after 1080*tbase, '0' after 1090*tbase, '0' after 1100*tbase, '1' after 1110*tbase, --0b01111101 (0x7D)
+    '0' after 1210*tbase, '0' after 1220*tbase, '0' after 1230*tbase, '0' after 1240*tbase, '0' after 1250*tbase, '0' after 1260*tbase, '0' after 1270*tbase, '0' after 1280*tbase, '0' after 1290*tbase, '0' after 1300*tbase, '1' after 1310*tbase, --00000000 (send UART) (0x00)
+    '0' after 1410*tbase, '1' after 1420*tbase, '0' after 1430*tbase, '0' after 1440*tbase, '0' after 1450*tbase, '1' after 1460*tbase, '0' after 1470*tbase, '1' after 1480*tbase, '0' after 1490*tbase, '1' after 1500*tbase, '1' after 1510*tbase, --0b01010001 (0x51)
+    '0' after 1610*tbase, '0' after 1620*tbase, '1' after 1630*tbase, '0' after 1640*tbase, '0' after 1650*tbase, '0' after 1660*tbase, '0' after 1670*tbase, '1' after 1680*tbase, '1' after 1690*tbase, '1' after 1700*tbase, '1' after 1710*tbase, --11000010 (set start value Timer) (0xC2)
+    '0' after 1810*tbase, '0' after 1820*tbase, '0' after 1830*tbase, '1' after 1840*tbase, '1' after 1850*tbase, '1' after 1860*tbase, '1' after 1870*tbase, '1' after 1880*tbase, '1' after 1890*tbase, '0' after 1900*tbase, '1' after 1910*tbase, --0b11111100 (0xFC)
+    '0' after 2010*tbase, '0' after 2020*tbase, '1' after 2030*tbase, '0' after 2040*tbase, '0' after 2050*tbase, '0' after 2060*tbase, '0' after 2070*tbase, '1' after 2080*tbase, '0' after 2090*tbase, '0' after 2100*tbase, '1' after 2110*tbase, --01000010 (restart Timer) (0x02)
+    '0' after 2210*tbase, '1' after 2220*tbase, '1' after 2230*tbase, '1' after 2240*tbase, '1' after 2250*tbase, '1' after 2260*tbase, '1' after 2270*tbase, '1' after 2280*tbase, '1' after 2290*tbase, '0' after 2300*tbase, '1' after 2310*tbase, --0b11111111 (0xFF)
+    '0' after 2410*tbase, '0' after 2420*tbase, '1' after 2430*tbase, '0' after 2440*tbase, '0' after 2450*tbase, '0' after 2460*tbase, '0' after 2470*tbase, '0' after 2480*tbase, '0' after 6490*tbase, '1' after 2500*tbase, '1' after 2510*tbase, --00000010 (enable Timer) (0x02)
+    '0' after 2610*tbase, '1' after 2620*tbase, '1' after 2630*tbase, '1' after 2640*tbase, '1' after 2650*tbase, '1' after 2660*tbase, '1' after 2670*tbase, '1' after 2680*tbase, '1' after 2690*tbase, '0' after 2700*tbase, '1' after 2710*tbase, --0b11111111 (0xFF)  
+    '0' after 3200*tbase, '0' after 3210*tbase, '1' after 3220*tbase, '0' after 3230*tbase, '0' after 3240*tbase, '0' after 3250*tbase, '0' after 3260*tbase, '0' after 3270*tbase, '0' after 3280*tbase, '1' after 3290*tbase, '1' after 3300*tbase, --00000010 (disable Timer) (0x02)
+    '0' after 3400*tbase, '0' after 3410*tbase, '0' after 3420*tbase, '0' after 3430*tbase, '0' after 3440*tbase, '0' after 3450*tbase, '0' after 3460*tbase, '0' after 3470*tbase, '0' after 3480*tbase, '0' after 3490*tbase, '1' after 3500*tbase; --0b00000000 (0x00)
+  
+  tb_rx_pin_a <= '1',
+    '0' after 100*tbase, '0' after 140*tbase, '0' after 180*tbase, '0' after 220*tbase, '0' after 260*tbase, '1' after 300*tbase, '0' after 340*tbase, '0' after 380*tbase, '0' after 420*tbase, '1' after 460*tbase; --0b00010000
+
   tb_gpio_pins_in <= "0",
-                      "1" after 50*tbase,
-                      "0" after 100*tbase;
+    "1" after 500*tbase,
+    "0" after 1100*tbase;
+
+  tb_exp_tx_pin_host <= '1',
+    '0' after 335*tbase, '1' after 345*tbase, '0' after 355*tbase, '0' after 365*tbase, '0' after 375*tbase, '0' after 385*tbase, '0' after 395*tbase, '0' after 405*tbase, '0' after 415*tbase, '1' after 425*tbase, '1' after 435*tbase, --0b00000001 (get GPIO data - unit) (0x01)
+    '0' after 445*tbase, '0' after 455*tbase, '0' after 465*tbase, '0' after 475*tbase, '0' after 485*tbase, '0' after 495*tbase, '0' after 505*tbase, '0' after 515*tbase, '0' after 525*tbase, '0' after 535*tbase, '1' after 545*tbase, --0b00000000 (get GPIO data - data) (0x00)
+    '0' after 555*tbase, '0' after 565*tbase, '0' after 575*tbase, '0' after 585*tbase, '0' after 595*tbase, '0' after 605*tbase, '0' after 615*tbase, '0' after 625*tbase, '0' after 635*tbase, '0' after 645*tbase, '1' after 655*tbase, --0b00000000 (UART in - unit) (0x00)
+    '0' after 665*tbase, '0' after 675*tbase, '0' after 685*tbase, '0' after 695*tbase, '0' after 705*tbase, '1' after 715*tbase, '0' after 725*tbase, '0' after 735*tbase, '0' after 745*tbase, '1' after 755*tbase, '1' after 765*tbase, --0b00010000 (UART in - data) (0x10)
+    '0' after 775*tbase, '1' after 785*tbase, '0' after 795*tbase, '0' after 805*tbase, '0' after 815*tbase, '0' after 825*tbase, '0' after 835*tbase, '0' after 845*tbase, '0' after 855*tbase, '1' after 865*tbase, '1' after 875*tbase, --0b00000001 (GPIO interrupt - unit) (0x01)
+    '0' after 885*tbase, '1' after 895*tbase, '0' after 905*tbase, '0' after 915*tbase, '0' after 925*tbase, '0' after 935*tbase, '0' after 945*tbase, '0' after 955*tbase, '0' after 965*tbase, '1' after 975*tbase, '1' after 985*tbase, --0b00000000 (GPIO interrupt - data) (0x01)
+    '0' after 1115*tbase, '1' after 1125*tbase, '0' after 1135*tbase, '0' after 1145*tbase, '0' after 1155*tbase, '0' after 1165*tbase, '0' after 1175*tbase, '0' after 1185*tbase, '0' after 1195*tbase, '1' after 1205*tbase, '1' after 1215*tbase, --0b00000001 (GPIO interrupt - unit) (0x01)
+    '0' after 1225*tbase, '0' after 1235*tbase, '0' after 1245*tbase, '0' after 1255*tbase, '0' after 1265*tbase, '0' after 1275*tbase, '0' after 1285*tbase, '0' after 1295*tbase, '0' after 1305*tbase, '0' after 1315*tbase, '1' after 1325*tbase, --0b00000000 (GPIO interrupt - data) (0x00)
+    '0' after 3115*tbase, '0' after 3125*tbase, '1' after 3135*tbase, '0' after 3145*tbase, '0' after 3155*tbase, '0' after 3165*tbase, '0' after 3175*tbase, '0' after 3185*tbase, '0' after 3195*tbase, '1' after 3205*tbase, '1' after 3215*tbase, --0b00000010 (Timer interrupt - unit) (0x02)
+    '0' after 3225*tbase, '1' after 3235*tbase, '1' after 3245*tbase, '1' after 3255*tbase, '1' after 3265*tbase, '1' after 3275*tbase, '1' after 3285*tbase, '1' after 3295*tbase, '1' after 3305*tbase, '0' after 3315*tbase, '1' after 3325*tbase; --0b11111111 (Timer interrupt - data) (0xFF)
+
+  tb_exp_tx_pin_a <= '1',
+    '0' after 1180*tbase, '1' after 1220*tbase, '0' after 1260*tbase, '1' after 1300*tbase, '1' after 1340*tbase, '1' after 1380*tbase, '1' after 1420*tbase, '1' after 1460*tbase, '0' after 1500*tbase, '1' after 1540*tbase, --0b01111101 (0x7D)
+    '0' after 1580*tbase, '1' after 1620*tbase, '0' after 1660*tbase, '0' after 1700*tbase, '0' after 1740*tbase, '1' after 1780*tbase, '0' after 1820*tbase, '1' after 1860*tbase, '0' after 1900*tbase, '1' after 1940*tbase; --0b01010001 (0x51)
+
+  tb_exp_gpio_pins_out <= "00",
+    "10" after 719*tbase;
+
+    tb_error <= '0' when 
+    (tb_exp_tx_pin_host = tb_tx_pin_host)
+    and (tb_exp_tx_pin_a = tb_tx_pin_a)
+    and (tb_exp_gpio_pins_out = tb_gpio_pins_out) else '1';
 
 end TESTBENCH;
